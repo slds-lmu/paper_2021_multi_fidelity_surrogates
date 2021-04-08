728748a27413c4eaf723de8c247336623b5c8126
pfistfl <pfistererf@googlemail.com>
2021-04-08 09:07:07 +0200

    helps

--- R/onnx_helpers.R
+++ R/onnx_helpers.R
@@ -18,3 +18,24 @@ run_with_mlflow = function(xs) {
   mlflow::mlflow_end_run()
   return(output)
 }
+
+# Map a character to the correct integer using a dict
+char_to_int = function(x, param_name, dict) {
+  x[is.na(x)] = "None"
+  matrix(dict[[param_name]][x,]$int)
+}
+
+# Predict using a .onnx file
+# inference time (1k samples):
+# - open session: 15 ms
+# - new session:  45 ms
+predict_onnx = function(model, data, dicts) {
+
+  li = c(
+    mlr3misc::imap(keep(data, is.character), char_to_int, dicts),
+    continuous = list(reticulate::r_to_py(as.matrix(keep(data, is.numeric)))$astype("float32"))
+  )
+  rt = reticulate::import("onnxruntime")
+  sess = rt$InferenceSession(model)
+  sess$run(NULL, li)[[1]]
+}
