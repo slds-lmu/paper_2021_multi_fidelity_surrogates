ab9a4bfe704161f59ace666ba3c2a1aa81e585db
pfistfl <pfistererf@googlemail.com>
2021-04-30 12:15:28 +0200

    fix task

--- tests/testthat/test_surrogate_predict.R
+++ tests/testthat/test_surrogate_predict.R
@@ -3,7 +3,7 @@ test_that("Objectives", {
   keys = cfgs()$keys()
   for (k in keys) {
     cfg = cfgs(k, workdir=workdir)
-    obj = cfg$get_objective()
+    obj = cfg$get_objective("3945")
     des = paradox::generate_design_random(cfg$param_set, 10)
     data = rbindlist(des$transpose(FALSE))[,names(obj$domain$params), with=FALSE]
     out = obj$eval_dt(data)
@@ -12,4 +12,17 @@ test_that("Objectives", {
     out = obj$eval_dt(data)
     expect_data_table(out, min.rows=10L)
   }
+})
+
+test_that("Objectives-Fix task", {
+  skip_if_not(check_directory_exists(workdir))
+  cfg = cfgs("lcbench", workdir=workdir)
+  obj = cfg$get_objective("3945")
+  des = paradox::generate_design_random(cfg$param_set, 10)
+  data = rbindlist(des$transpose(FALSE))[,names(obj$domain$params), with=FALSE]
+  out = obj$eval_dt(data)
+  expect_data_table(out, min.rows=10L)
+  data = data[, sample(seq_along(data)), with = FALSE]
+  out = obj$eval_dt(data)
+  expect_data_table(out, min.rows=10L)
 })
