afc3f38c9fb195b4cff5709cd031f8a70fd62297
sumny <lennart.sch@web.de>
2021-04-30 18:51:19 +0200

    ..

--- R/BenchmarkConfigs.R
+++ R/BenchmarkConfigs.R
@@ -840,7 +840,6 @@ BenchmarkConfigFCNet = R6Class("BenchmarkConfigFCNet",
         target_variables = c("valid_loss", "runtime", "n_params"),
         codomain = ps(
           valid_loss = p_dbl(lower = 0, upper = 1, tags = "minimize"),
-          valid_mse = p_dbl(lower = 0, upper = 1, tags = "minimize"),
           runtime = p_dbl(lower = 0, upper = Inf, tags = "minimize"),
           n_params = p_dbl(lower = 0, upper = Inf, tags = "minimize")
         ),
@@ -852,7 +851,6 @@ BenchmarkConfigFCNet = R6Class("BenchmarkConfigFCNet",
     param_set = function() {
       ps(
         epoch = p_int(lower = 1L, upper = 100L, tags = "budget"),
-        replication = p_int(lower = 1L, upper = 4L, tags = "budget"),
         activation_fn_1 = p_fct(levels = c("relu", "tanh")),
         activation_fn_2 = p_fct(levels = c("relu", "tanh")),
         batch_size = p_int(lower = 8L, upper = 64L),
