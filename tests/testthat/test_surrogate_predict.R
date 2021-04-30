c8ed5a322f742fab845fc1f554c5c2dcacc43b89
pfistfl <pfistererf@googlemail.com>
2021-04-30 12:16:32 +0200

    typo

--- tests/testthat/test_surrogate_predict.R
+++ tests/testthat/test_surrogate_predict.R
@@ -3,7 +3,7 @@ test_that("Objectives", {
   keys = cfgs()$keys()
   for (k in keys) {
     cfg = cfgs(k, workdir=workdir)
-    obj = cfg$get_objective("3945")
+    obj = cfg$get_objective()
     des = paradox::generate_design_random(cfg$param_set, 10)
     data = rbindlist(des$transpose(FALSE))[,names(obj$domain$params), with=FALSE]
     out = obj$eval_dt(data)
