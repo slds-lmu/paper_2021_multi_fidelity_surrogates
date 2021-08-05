d7ad0b072fde993ff645955f7691143e3e18500b
sumny <lennart.sch@web.de>
2021-08-05 12:41:08 +0200

    ..

--- R/preproc_synthetics.R
+++ R/preproc_synthetics.R
@@ -7,9 +7,8 @@ preproc_branin_surrogate = function(config, seed = 123L, n_max = 2*10^6, frac = 
   # Preproc train data
   train = tt$train
   train = preproc_iid(train)
-  train = sample_max(train, n_max)
   trafos = c(
-    map(train[, "y", with = FALSE], scale_base_0_1, base = 1, p = 0)
+    map(train[, "y", with = FALSE], scale_base_0_1, base = 10, p = 0)
   )
   train[, names(trafos) := pmap(list(.SD, trafos), function(x, t) {t$trafo(x)}), .SDcols = names(trafos)]
   y = as.matrix(train[, config$target_variables, with = FALSE])
@@ -45,7 +44,6 @@ preproc_hartmann_surrogate = function(config, seed = 123L, n_max = 2*10^6, frac 
   # Preproc train data
   train = tt$train
   train = preproc_iid(train)
-  train = sample_max(train, n_max)
   trafos = c(
     map(train[, "y", with = FALSE], scale_base_0_1, base = 1, p = 0)
   )
