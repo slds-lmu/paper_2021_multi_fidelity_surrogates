1e0888b003429e416e24b0162d11cd9a2e92c3c1
user <lennart.sch@web.de>
2021-08-10 23:11:11 +0200

    preproc phoneme

--- R/preproc_synthetics.R
+++ R/preproc_synthetics.R
@@ -167,7 +167,7 @@ preproc_rpartphoneme_surrogate = function(config, seed = 123L, n_max = 2*10^6, f
   trafos = c(
     map(train[, "y", with = FALSE], scale_base_0_1, base = 10, p = 0),
     map(train[, "cp", with = FALSE], scale_base_0_1, base = 1, p = 0),
-    map(train[, "maxdepth", with = FALSE], scale_base_0_1, base = 1, p = 0),
+    map(train[, "maxdepth", with = FALSE], scale_base_0_1, base = 1, p = 0)
   )
   train[, names(trafos) := pmap(list(.SD, trafos), function(x, t) {t$trafo(x)}), .SDcols = names(trafos)]
   y = as.matrix(train[, config$target_variables, with = FALSE])
