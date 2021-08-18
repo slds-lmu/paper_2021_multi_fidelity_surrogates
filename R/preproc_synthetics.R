3bb6ce396b2ed1fa5c41fae47f2a82ac05910d57
sumny <lennart.sch@web.de>
2021-08-18 10:28:13 +0200

    ..

--- R/preproc_synthetics.R
+++ R/preproc_synthetics.R
@@ -159,17 +159,17 @@ preproc_rpart_surrogate = function(config, seed = 123L, n_max = 2*10^6, frac = .
   path = config$data_path
   dt = readRDS(path)
   # We get rid of larger loss in training. This leads to mathematical instabilities otherwise.
-  upper_outliers = which(dt$y > 1)
-  if (length(upper_outliers)) {
-    dt = dt[-upper_outliers, ]
-  }
+  #upper_outliers = which(dt$y > 1)
+  #if (length(upper_outliers)) {
+  #  dt = dt[-upper_outliers, ]
+  #}
   tt = split_by_col(dt, by = NULL, frac = 0.1)
 
   # Preproc train data
   train = tt$train
   train = preproc_iid(train)
   trafos = c(
-    map(train[, "cp", with = FALSE], scale_base_0_1, base = 1, p = 0),
+    map(train[, "y", with = FALSE], scale_base_0_1, base = 10, p = 0),
     map(train[, "maxdepth", with = FALSE], scale_base_0_1, base = 1, p = 0)
   )
   train[, names(trafos) := pmap(list(.SD, trafos), function(x, t) {t$trafo(x)}), .SDcols = names(trafos)]
@@ -202,17 +202,17 @@ preproc_glmnet_surrogate = function(config, seed = 123L, n_max = 2*10^6, frac = 
   path = config$data_path
   dt = readRDS(path)
   # We get rid of larger loss in training. This leads to mathematical instabilities otherwise.
-  upper_outliers = which(dt$y > 1)
-  if (length(upper_outliers)) {
-    dt = dt[-upper_outliers, ]
-  }
+  #upper_outliers = which(dt$y > 1)
+  #if (length(upper_outliers)) {
+  #  dt = dt[-upper_outliers, ]
+  #}
   tt = split_by_col(dt, by = NULL, frac = 0.1)
 
   # Preproc train data
   train = tt$train
   train = preproc_iid(train)
   trafos = c(
-    map(train[, "alpha", with = FALSE], scale_base_0_1, base = 1, p = 0),
+    map(train[, "y", with = FALSE], scale_base_0_1, base = 10, p = 0),
     map(train[, "s", with = FALSE], scale_base_0_1, base = 1, p = 0)
   )
   train[, names(trafos) := pmap(list(.SD, trafos), function(x, t) {t$trafo(x)}), .SDcols = names(trafos)]
